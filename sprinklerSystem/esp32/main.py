4849519d48f27cbd768296fba99919c9f001cc06
aparajita-gupta <asthu0907@gmail.com>
2022-08-20 12:50:52 -0700

    Added missing files in last commit

--- sprinklerSystem/esp32/main.py
+++ sprinklerSystem/esp32/main.py
@@ -1,11 +1,13 @@
 import sys
 sys.path.append('./app')
 import schedule_task
-#import sprinkler
+import utils
 import gc
+import lib.myntptime
 #import webrepl
 #webrepl.start()
 gc.collect()
-
+utils.connectToWifi()
+lib.myntptime.init()
 schedule_task.smain()
 #sprinkler.init()
